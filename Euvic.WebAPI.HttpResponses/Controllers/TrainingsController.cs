46a5a258e7423eb399e22d2734adf30bfbf057a8
Kamil Paszkowski <Kamil.Paszkowski@euvic.pl>
2022-12-04 11:55:02 +0100

    add [?] token

--- Euvic.WebAPI.HttpResponses/Controllers/TrainingsController.cs
+++ Euvic.WebAPI.HttpResponses/Controllers/TrainingsController.cs
@@ -19,35 +19,39 @@ namespace Euvic.WebAPI.HttpResponses.Controllers
         // NotFound() 404
         // ----------------------------------------------------------------------------------------------------------------
 
+        // ten endpoint jest gotowy
         // endpoint powinien zwrócic obiekt TrainingsList więc została zastosowana tutaj metoda Ok()
         [HttpGet("")]
         public IActionResult GetTrainings() => Ok(new TrainingsList());
 
+        // endpointy poniżej są do zrobienia
+        // ----------------------------------------------------------------------------------------------------------------
+
         // endpoint ma zwrócic kolekcję obiektów "Models.Attendee"
         [HttpGet("{id}/attendees")]
-        public IActionResult GetTrainingAttendees() =>?;
+        public IActionResult GetTrainingAttendees() => [?];
 
         // endpoint powinien zwrócić że udało się stworzyć trening i ten trening zostaje zwrócony w odpowiedzi (obiekt Models.Training)
         [HttpPost("")]
-        public IActionResult CreateTraining() => ?;
+        public IActionResult CreateTraining() =>  [?];
 
         // endpoint powinien zwrócić że użytkownik ma nieważny token służący do uwierzytelnienia
         [HttpPost("")]
-        public IActionResult CreateTrainingBadToken() => ?;
+        public IActionResult CreateTrainingBadToken() =>  [?];
 
         // endpoint powinien zwrócić że użytkownik ma ważny token lecz nie ma uprawnień w aplikacji aby wykonać ten request
         [HttpDelete("{id}")]
-        public IActionResult DeleteTraining([FromRoute] int id) =>?;
+        public IActionResult DeleteTraining([FromRoute] int id) => [?];
 
         [HttpDelete("{id}/attendee")]
-        public IActionResult DeleteAttendee([FromRoute] int id) => ?;
+        public IActionResult DeleteAttendee([FromRoute] int id) => [?];
 
         // endpoint ma zwrócic odpowiedź że update się udał lecz nic nie zostaje zwrócone
         [HttpPut("{id}")]
-        public IActionResult UpdateTraining([FromRoute] int id, [FromBody] UpdateTrainingRequest request) => ?;
+        public IActionResult UpdateTraining([FromRoute] int id, [FromBody] UpdateTrainingRequest request) =>  [?];
 
-        // endpoint ma zwrócic odpowiedź że nie udało się zmienić "duration" ponieważ wartość durationInMinutes jest nieprawidłowa
+        // endpoint ma zwrócic odpowiedź że nie udało się zmienić "duration"
         [Route("{id}/duration")]
-        public IActionResult ChangeTrainingDuration([FromRoute] int id, [FromBody] int durationInMinutes) => ?;
+        public IActionResult ChangeTrainingDuration([FromRoute] int id, [FromBody] int durationInMinutes) =>  [?];
     }
 }
